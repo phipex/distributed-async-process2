4c8035361e24b89fa543a6450c59fbcbfb54d284
andres.montoya <andres.montoya@iesonline.com.co>
2021-08-12 16:44:17 -0500

    feat: se agregan tareas pendientes por procesar a redis

--- webservice/src/main/java/co/com/ies/pruebas/webservice/redis/ServiceProcessImpl.java
+++ webservice/src/main/java/co/com/ies/pruebas/webservice/redis/ServiceProcessImpl.java
@@ -21,8 +21,8 @@ public class ServiceProcessImpl implements ServiceProcess {
     }
 
     @Async
-    @Override
-    public void process() {
+    @Deprecated
+    public void process2() {
         System.out.println("ServiceProcessQeueuImpl.processQeueu");
         try {
             String hostAddress = InetAddress.getLocalHost().getHostAddress();
@@ -52,6 +52,16 @@ public class ServiceProcessImpl implements ServiceProcess {
             e.printStackTrace();
         }
         
+    }
+
+    @Async
+    @Override
+    public void process() {
+        final List<Greeting> greetingsByIpTramitedIsNull = greetingRepository.getGreetingsByIpTramitedIsNull();
+
+        qeueuAsyncRedis.offerTascks(greetingsByIpTramitedIsNull);
+        final int size = greetingsByIpTramitedIsNull.size();
+        System.out.println("size = " + size);
     }
 
     @Async
